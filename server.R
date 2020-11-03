12aadffbf481f366ae7903f01e249dbfecab423c
khaled Fallatah <68388272+fallatahkm@users.noreply.github.com>
2020-11-03 21:49:01 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 shinyServer(function(input, output) {
     output$plot1 <- renderPlot({
@@ -9,31 +10,42 @@ shinyServer(function(input, output) {
         number_sd <- input$numeric_sd
         minx <- input$sliderx[1]
         maxx <- input$sliderx[2]
-        datax<- rnorm(number_of_point,number_avg, number_sd)
-        xlab <- ifelse(input$show_xlab," X Axis", "")
-        ylab <- ifelse(input$show_ylab, "Y Axis", "")
-        main <- ifelse(input$show_title, "Title", "")
-        hist(datax, xlab = xlab, ylab = ylab, main = main)
-        abline(v = minx, col= "green", wld =6); abline(v = maxx, col= "red", wld =6)
+        datax <- rnorm(number_of_point,number_avg, number_sd)
+        
+        xlab <- ifelse(input$show_xlab," Process Distribution", "")
+        ylab <- ifelse(input$show_ylab, "Frquency", "")
+        main <- ifelse(input$show_title, "Graph of Performance Plotting, According to Customer Specification", "")
+        hist(datax , xlab = xlab, ylab = ylab, main = main, xlim = c(0, 500))
+        abline(v = minx, col= "green", lwd = 3); abline(v = maxx, col= "red", lwd = 3)
+    
     })
     output$plot2 <- renderPlot({
         set.seed(2020)
         number_of_point <- input$numeric
         number_avg <- input$numeric_a
         number_sd <- input$numeric_sd
+        output$out1 <- renderText(input$box1)
+        output$out2 <- renderText(input$box2)
+    
         minx <- input$sliderx[1]
         maxx <- input$sliderx[2]
         datax<- rnorm(number_of_point,number_avg, number_sd)
-        xlab <- ifelse(input$show_xlab," X Axis", "")
-        ylab <- ifelse(input$show_ylab, "Y Axis", "")
-        main <- ifelse(input$show_title, "Title", "")
-        plot(datax, xlab = xlab, ylab = ylab, main = main)
-        abline(h = c(-3,3)*input$numeric_sd+input$numeric_a, col= "blue", wld =10)
+        xlab <- ifelse(input$show_xlab,"Frquency", "")
+        ylab <- ifelse(input$show_ylab, "Process Distribution", "")
+        main <- ifelse(input$show_title, "Graph of Performance Plotting, Accoding to Statistical Specification", "")
+        plot(datax, col = "red", ylab = ylab,xlab = xlab, main = main)
+        abline(h = c(-3,3)*input$numeric_sd+input$numeric_a, col= "blue", lwd =3)
+        
     })
     output$result <- renderText({
-        
-        fit <- 1-diff(pnorm(c(input$sliderx[1],input$sliderx[2]),input$numeric_a,input$numeric_sd))
-        res <- paste(round(fit*1000000, digits = 1.5),"DPMO" )
+      
+       fit <- 1-diff(pnorm(c(input$sliderx[1],input$sliderx[2]),input$numeric_a,input$numeric_sd))
+       
+       fitx <- eventReactive(input$go, {
+         fit
+       })
+      
+        res <- paste(round(fitx()*1000000, digits = 1.5),"DPMO" )
         res
     })
 })
--- ui.R
+++ ui.R
@@ -1,27 +1,54 @@
 library(shiny)
 shinyUI(fluidPage(
-    titlePanel("Processes Performace App"),
+    titlePanel("Process Performace DPMO App"),
     sidebarLayout(
         sidebarPanel(
-            numericInput("numeric", " Identifid the required to generat?",
+            numericInput("numeric", "Identify the required number to be generated",
                          value = 1000, min = 100, max = 1000, step = 100),
-            numericInput("numeric_a", " identifid the process Average?",
-                         value = 100, min = 1, max = 1000, step = 10),
-            numericInput("numeric_sd", " identifid the process Stanad. Dev.?",
-                         value = 10, min = 1, max = 1000, step = 1),
-            sliderInput("sliderx", "Identify the Customer sepecification?", 0 , 300,
-                        value = c(80,120)),
+            numericInput("numeric_a", "Identify the process's Average",
+                         value = 200, min = 1, max = 500, step = 10),
+            numericInput("numeric_sd", "Identify the process's Stanad. Dev.",
+                         value = 40, min = 1, max = 1000, step = 1),
+            sliderInput("sliderx", "Identify the upper and lower customer requirement", 0 , 1000,
+                        value = c(150,250)),
             checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
-            checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
-            checkboxInput("show_title", "Show/Hide Title")
+            checkboxInput("show_ylab", "Show/Hide y Axis Label", value = TRUE),
+            checkboxInput("show_title", "Show/Hide Title", value = TRUE),
+            
+         
+            actionButton("go", "Go!")
+        
+            
         ),
+        
         mainPanel(
-            h3("Graph of Performance Plotting, According to Customer Specification"),
-            plotOutput("plot1"),
-            h3("Graph of Performance Plotting, Accoding to Statistical Specification"),
-            plotOutput("plot2"),
-            h4("Predicted Process Defects Per Milloin Opportuenties is:"),
-            h3(textOutput("result"))
-        )
+            h4("Predicted Process Defects Per Million Opportunities is:"), 
+            h5(textOutput("result"),
+               
+            tabsetPanel(type = "tabs",
+                tabPanel("By Customer Specification ", br(
+                    plotOutput("plot1"),
+                  
+                    ), textOutput("out1")),
+                tabPanel("By Statistical Specification", br(
+                  
+                    plotOutput("plot2")
+                    ), textOutput("out2")),
+                 tabPanel("Summary Discription", br(), p("- This application is to predict the process performance by
+                 understanding process parameter i.e. Mean, stander deviation."),
+                          p("- First, by Customer Specification, this is mean adjusting the upper and lower limits according customer need or expect from process."),
+                          p('- Second, By Statistical Specification, this is mean the upper and lower limits allocated according to 3 standers deviation from the mean.
+                 and the out-limitation point is considered as signals.'),
+                          p("- Third, the use must fill all blanket accordingly . the maximum number can be generated is 1000, prosses Average is 500 and stander deviation up to 100."),
+                          p("- fourth, press the action bottom the result of Defect Per million at the top of page.")
+                                                
+                                                    , textOutput("out3")),
+                
+                tabPanel(" Code Summary", br(),
+                         p("applicaiton core: "),textOutput("out4"))
+                       
+                
+                )
+            )
     )
-))
+)))
